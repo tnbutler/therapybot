7b68e40c6c96e0ca4773f33cc28ad2c81256f1df
Dmitry Sapzhev <dspazhev@gmail.com>
2016-11-11 14:38:26 +0400

    Set up routing & controller for the DemoApi endpoint.

---
+++ server-api/app/Http/Controllers/ApiAdapters/DemoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\ApiAdapters;
+
+use App\Http\Controllers\Controller;
+
+class DemoController extends Controller
+{
+    public function processWebHookCall()
+    {
+        return 123;
+    }
+}
