2bca5bb8a484d8f41d517acefcf56ae1bac3942a
Dmitry Sapzhev <dspazhev@gmail.com>
2016-11-25 09:20:36 +0400

    Demo interface - interim.

--- admin-panel/app/app.module.ts
+++ admin-panel/app/app.module.ts
@@ -7,6 +7,7 @@ import {AppComponent}  from './app.component';
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroesComponent}     from './heroes.component';
 import {HeroService}         from './hero.service';
+import {DashboardComponent}         from './dashboard.component';
 
 @NgModule({
     imports: [
@@ -16,12 +17,22 @@ import {HeroService}         from './hero.service';
             {
                 path: 'heroes',
                 component: HeroesComponent
-            }
+            },
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: '',
+                redirectTo: '/dashboard',
+                pathMatch: 'full'
+            },
         ])
     ],
     declarations: [
         AppComponent,
         HeroDetailComponent,
+        DashboardComponent,
         HeroesComponent,
     ],
     providers: [
