3187853203aa0e0e057c94195f837d577cd7f464
Dmitry Sapzhev <dspazhev@gmail.com>
2016-12-07 12:49:18 +0400

    Fix OPTION-request caused issue

--- server-api/routes/web.php
+++ server-api/routes/web.php
@@ -4,4 +4,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::match(['get', 'post', 'options'], 'demoApi', 'ApiAdapters\DemoController@processWebHookCall');
+Route::match(['get', 'post'], 'demoApi', 'ApiAdapters\DemoController@processWebHookCall');
+
+// Angular2 application performs OPTION-calls for the cross-origin requests.
+// We catch them here, and return nothing.
+Route::options('demoApi', function () {
+    header("Access-Control-Allow-Origin: *");
+    header('Access-Control-Allow-Headers: Content-Type');
+    return "";
+});
