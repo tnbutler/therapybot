a059dfd7ff15d325bd04d40aa5f5c80870971f29
Dmitry Sapzhev <dspazhev@gmail.com>
2016-12-13 14:08:00 +0400

    Refactored duplicated code.

--- server-api/app/Models/ChatNode.php
+++ server-api/app/Models/ChatNode.php
@@ -8,6 +8,8 @@ class ChatNode extends Model
 {
     public $timestamps = false;
     protected $hidden = ['chat_version_id'];
+    const SYS_VAR_PREFIX = '[@';
+    const SYS_VAR_POSTFIX = '@]';
 
     public function answerButtons()
     {
@@ -16,30 +18,26 @@ class ChatNode extends Model
 
     public function getTextWithUserVariableValues(BotUser $botUser)
     {
-        $result = $this->question_text;
-
-        $replaces = UserVariableValue::where('bot_users_id', $botUser->id)
-            ->orderBy('updated_at', 'desc')
-            ->get();
-
-        foreach ($replaces as $replace) {
-            $searchString = '[@' . $replace->user_variable_id . '@]';
-            $replaceString = $replace->value;
-            $result = str_replace($searchString, $replaceString, $result);
-        }
-
-        return $result;
+        $replaces = UserVariableValue::where('bot_users_id', $botUser->id)->orderBy('updated_at', 'desc')->get();
+        return $this->_performReplaces($replaces);
     }
 
     public function getTextWithUserVariableSysNames()
     {
+        $replaces = UserVariable::where('chat_version_id', $this->chat_version_id)->get();
+        return $this->_performReplaces($replaces, '@');
+    }
+
+    private function _performReplaces($replaces, $prefixAndPostfix = null)
+    {
         $result = $this->question_text;
 
-        $replaces = UserVariable::where('chat_version_id', $this->chat_version_id)->get();
-
         foreach ($replaces as $replace) {
-            $searchString = '[@' . $replace->id . '@]';
-            $replaceString = '@' . $replace->name . '@';
+            $searchString = self::SYS_VAR_PREFIX . $replace->id . self::SYS_VAR_POSTFIX;
+            $replaceString = $replace->name;
+            if ($prefixAndPostfix) {
+                $replaceString = $prefixAndPostfix . $replaceString . $prefixAndPostfix;
+            }
             $result = str_replace($searchString, $replaceString, $result);
         }
 
